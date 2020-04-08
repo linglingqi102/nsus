1803157a01031e00e8bce2d4005d0ba1aa46e2bb
xumin <xumin@xumindeMacBook-Air.local>
2020-04-08 17:09:29 +0800

    xm add

--- src/pages/meeting/meeting.js
+++ src/pages/meeting/meeting.js
@@ -1,10 +1,40 @@
 
-// import styles from './meeting.css';
+import styles from './meeting.less';
 import ComBox from '../../components/ComBox'
 
 export default function () {
   return (
     <ComBox title="会议日程">
+      <div className = {styles.news}>
+        <p className = {styles.titlename}><strong>会议总体日程</strong></p>
+        <table className = {styles.table} align="center" border="1" cellPadding="1" cellSpacing="1 ">
+          <tbody>
+            <tr>
+              <td rowSpan="2">7月31日</td>
+              <td>12:00 - 22:00</td>
+              <td>报到</td>
+            </tr>
+            <tr>
+              <td>12:00 - 22:00</td>
+              <td>全天注册</td>
+            </tr>
+            <tr>
+              <td rowSpan="2">7月15日</td>
+              <td>12:00 - 22:00</td>
+              <td>报到</td>
+            </tr>
+            <tr>
+              <td>12:00 - 22:00</td>
+              <td>会议报告及海报展示</td>
+            </tr>
+          </tbody>
+        </table>
+        <p>
+          <a href="#" target="_blank">
+            <span>点击此处查看会议日程</span>
+          </a>
+        </p>
+      </div>
     </ComBox>
   );
 }
--- src/pages/organization/organization.js
+++ src/pages/organization/organization.js
@@ -1,10 +1,109 @@
 
-// import styles from './organization.css';
+import styles from './organization.less';
 import ComBox from '../../components/ComBox'
+import Item from 'antd/lib/list/Item';
 
 export default function () {
   return (
     <ComBox title="组织机构">
+      <div className = {styles.news}>
+        <p className = {styles.titlename}><strong>组织机构</strong></p>
+        <p className = {styles.title}><strong>大会主席</strong></p>
+        <div className = {styles.first}>
+          <div className = {styles.item_first}>
+            <img src={require('root/assets/lps.jpg')} alt="lps"></img>
+            <p>龚旗煌</p>
+            <p>北京大学</p>
+          </div>
+          <div className = {styles.item_first}>
+            <img src={require('root/assets/lps.jpg')} alt="lps"></img>
+            <p>龚旗煌</p>
+            <p>北京大学</p>
+          </div>
+          <div className = {styles.item_first}>
+            <img src={require('root/assets/lps.jpg')} alt="lps"></img>
+            <p>龚旗煌</p>
+            <p>北京大学</p>
+          </div>
+        </div>
+        <p className = {styles.title}><strong>组织委员会主席</strong></p>
+        <div className = {styles.first}>
+          <div className = {styles.item_first}>
+            <img src={require('root/assets/lps.jpg')} alt="lps"></img>
+            <p>吴健</p>
+            <p>华东师范大学</p>
+          </div>
+          <div className = {styles.item_first}>
+            <img src={require('root/assets/lps.jpg')} alt="lps"></img>
+            <p>翁羽翔</p>
+            <p>中国科学院物理研究所</p>
+          </div>
+        </div>
+        <p className = {styles.title}><strong>会议专业委员会</strong></p>
+        <table className = {styles.table} align="center" border="0" cellPadding="3" cellSpacing="3">
+          <tbody>
+            <tr>
+              <td>崔执凤</td>
+              <td>丁大军</td>
+              <td>方维海</td>
+              <td>高毅勤</td>
+              <td>龚旗煌</td>
+            </tr>
+            <tr>
+              <td>崔执凤</td>
+              <td>丁大军</td>
+              <td>方维海</td>
+              <td>高毅勤</td>
+              <td>龚旗煌</td>
+            </tr>
+            <tr>
+              <td>崔执凤</td>
+              <td>丁大军</td>
+              <td>方维海</td>
+              <td>高毅勤</td>
+              <td>龚旗煌</td>
+            </tr>
+            <tr>
+              <td>崔执凤</td>
+              <td>丁大军</td>
+              <td>方维海</td>
+              <td>高毅勤</td>
+              <td>龚旗煌</td>
+            </tr>
+            <tr>
+              <td>崔执凤</td>
+              <td>丁大军</td>
+              <td>方维海</td>
+              <td>高毅勤</td>
+              <td>龚旗煌</td>
+            </tr>
+            <tr>
+              <td>崔执凤</td>
+              <td>丁大军</td>
+              <td>方维海</td>
+              <td>高毅勤</td>
+              <td>龚旗煌</td>
+            </tr>
+          </tbody>
+        </table>
+        <p className = {styles.title}><strong>组织委员会</strong></p>
+        <table className = {styles.table} align="center" border="0" cellPadding="3" cellSpacing="3">
+          <tbody>
+            <tr>
+              <td>程&nbsp;&nbsp;亚</td>
+              <td>丁大军</td>
+              <td>方维海</td>
+              <td>高毅勤</td>
+              <td className ={styles.tdwidth}>Konstantin&nbsp;E. Dorfman</td>
+            </tr>
+          </tbody>
+        </table>
+        <p className = {styles.title}><strong>承办单位</strong></p>
+        <p className={styles.unitimg}>
+          <img src={require('root/assets/lps.jpg')} alt="lps"></img>
+        </p>
+        <p className={styles.unit}>华东师范大学 精密光谱科学与技术国家重点实验室</p>
+      </div>
     </ComBox>
   );
 }
