f34d33ae1a1be52637bf6da3722706a7ef2f37f6
liupeng <1050691838@qq.com>
2020-04-09 18:21:18 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 remote=root@47.94.239.228
-ssh $remote "cd /www/nsus; git pull; yarn; yarn build; pm2 restart nsus"
+ssh $remote "cd /www/nsus; git pull; yarn; yarn build; cd /home; ./ossutil cp -r --update oss://nsus-assets /www/nsus/dist; pm2 restart nsus"
