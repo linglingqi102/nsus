64bfd1ee8c7e250c42db25aa9f4a9c6c48000b68
liupeng <liupeng@aixuexi.com>
2021-04-18 11:04:27 +0800

    feat: 去掉名字

--- src/components/ComBox.jsx
+++ src/components/ComBox.jsx
@@ -5,13 +5,6 @@ export default function ComBox(props) {
     return (
         <div className={styles.box}>
             <div className={styles.title}>{props.title}</div>
-            <div className={styles.msg}>
-                <div className={styles.time}>来源：张洁&nbsp;&nbsp;&nbsp;&nbsp;发布时间：2020-07-23 14:11</div>
-                <div className={styles.share}>
-                    <span>分享到：</span>
-                    <img className={styles.b_weixin} src={require('../assets/weixin.gif')} alt="分享到更多" />
-                </div>
-            </div>
             <div className={styles.content}>
                 {props.children}
             </div>
