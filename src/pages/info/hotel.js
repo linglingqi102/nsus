14567d8e813d6a77f41721e1e17c3fd4b3c900e7
liupeng <liupeng@aixuexi.com>
2021-05-12 10:36:05 +0800

    feat: 添加酒店交通

--- src/pages/info/hotel.js
+++ src/pages/info/hotel.js
@@ -1,10 +1,34 @@
 
-// import styles from './hotel.css';
+import styles from './hotel.less';
 import ComBox from '../../components/ComBox'
-
+const URI = [
+  'http://nsus-static.oss-cn-beijing.aliyuncs.com/hotel1.png',
+  'http://nsus-static.oss-cn-beijing.aliyuncs.com/hotel2.png',
+  'http://nsus-static.oss-cn-beijing.aliyuncs.com/hotel3.png',
+]
 export default function () {
   return (
     <ComBox title="酒店信息">
+      <div className={styles.content}>
+        <p style={{marginBottom: 30}}>本次会议拟定于2021年7月18日-21日在“<span className={styles.textBlue}>青岛国际院士港大酒店</span>”举办。</p>
+        <p>会务组已在下列酒店以优惠价格为参会代表预留一定数量的房间，请通过<a href="https://www.wjx.cn/vj/YHyjRrz.aspx" rel="noreferrer" target="_blank">https://www.wjx.cn/vj/YHyjRrz.aspx</a>链接进行酒店入住信息登记。</p>
+        <p>1. 青岛国际院士港大酒店（青岛市李沧区金水路171号院）</p>
+        <ul>
+          <li>豪华双床/高级大床房：480元/间 含早</li>
+          <li>雅致双床/大床房：430元/间 含早</li>
+        </ul>
+        <p>2. 青岛国际院士港信潮大酒店（青岛市崂山区松岭路395号）</p>
+        <ul style={{marginBottom: 30}}>
+          <li>雅致双床/大床房：400元/间 含早</li>
+        </ul>
+        <p>联系人：张洁  0532-58632168/13061370150</p>
+        <p className={styles.textBlue}>注：会议住宿费由参会者自行与酒店结算。由于7月21日退房时间集中，请参会代表在当天尽量提前去酒店前台开具发票，避免耽误您的行程。</p>
+        <div className={styles.imgBox}>
+          <img src={URI[0]} alt="" />
+          <img src={URI[1]} alt=""/>
+          <img src={URI[2]} alt=""/>
+        </div>
+      </div>
     </ComBox>
   );
 }
--- src/pages/info/traffic.js
+++ src/pages/info/traffic.js
@@ -1,10 +1,29 @@
 
-// import styles from './traffic.css';
+import styles from './traffic.less';
 import ComBox from '../../components/ComBox'
-
+const URI = [
+  'http://nsus-static.oss-cn-beijing.aliyuncs.com/map.png',
+]
 export default function () {
   return (
     <ComBox title="交通信息">
+      <div className={styles.content}>
+        <p style={{marginBottom: 30}}>本次会议拟定于2021年7月18日-21日在“<span className={styles.textBlue}>青岛国际院士港大酒店</span>”举办。</p>
+        <p>1. 青岛北站</p>
+        <p>方案一：在铁路北站东广场站乘坐387路公交车至李家上流小区站下车，再步行到达酒店，用时约1小时。</p>
+        <p>方案二：在青岛北站地铁站乘坐3号线至李村站下车，再从夏庄路向阳路站乘坐128路公交车至李家上流小区站下车，后步行到达酒店，用时约1小时。</p>
+        <p>方案三：出租车约30元，用时约25分钟，距离约12公里。</p>
+        <p>2. 青岛流亭国际机场</p>
+        <p>方案一：在流亭站乘坐地铁1号线至青岛北站，再换乘地铁3号线至李村站下车，然后从夏庄路向阳路站乘坐128路公交车至李家上流小区站，后步行到达酒店，用时约1小时30分钟。
+        </p>
+        <p>方案二：出租车约50元，用时约40分钟，距离约20公里。</p>
+        <p>3. 青岛站</p>
+        <p>方案一：在青岛站地铁站乘坐3号线至李村站下车，再从夏庄路向阳路站乘坐128路公交车至李家上流小区站下车，后步行到达酒店，用时约1小时30分钟。</p>
+        <p>方案二：出租车约70元，用时约50分钟，距离约25公里。</p>
+        <div className={styles.imgBox}>
+          <img src={URI[0]} alt="" />
+        </div>
+      </div>
     </ComBox>
   );
 }
